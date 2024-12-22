357a27cb9a437db9fb211c590d0de48623e3f120
Izzet-Agayev <agayevizzet56@gmail.com>
2024-12-23 00:23:36 +0400

    Class base views

--- watches/urls.py
+++ watches/urls.py
@@ -5,10 +5,21 @@ from watches import views as watches_views
 
 # urls.py views.py fayillari elaqelendirilir
 urlpatterns = [
-    path('', watches_views.all_watches, name='all_watch'),    # 127.0.0.1:8000/watches/
-    path('create', watches_views.create_watch, name='watch_create'),    # 127.0.0.1:8000/create/
-    path('my_watch', watches_views.my_watches, name='my_watch'),    # 127.0.0.1:8000/my_watches/
-    path('detail/<int:pk>/', watches_views.detail_watch, name='watch_detail'),    # 127.0.0.1:8000/detail/<int:pk>/
-    path('update/<int:pk>/', watches_views.update_watch, name='watch_update'),    # 127.0.0.1:8000/update/<int:pk>/
-    path('delete/<int:pk>/', watches_views.delete_watch, name='watch_delete'),    # 127.0.0.1:8000/delete/<int:pk>/
+    # path('', watches_views.all_watches, name='all_watch'),    # 127.0.0.1:8000/watches/
+    path('', watches_views.ListWatchView.as_view(), name='all_watch'),    # 127.0.0.1:8000/watches/
+    
+    # path('my_watch', watches_views.my_watches, name='my_watch'),    # 127.0.0.1:8000/my_watches/
+    path('my_watch', watches_views.MyWatchView.as_view(), name='my_watch'),    # 127.0.0.1:8000/my_watches/
+
+    # path('create', watches_views.create_watch, name='watch_create'),    # 127.0.0.1:8000/create/
+    path('create', watches_views.CreateWatchView.as_view(), name='watch_create'),    # 127.0.0.1:8000/create/
+    
+    # path('detail/<int:pk>/', watches_views.detail_watch, name='watch_detail'),    # 127.0.0.1:8000/detail/<int:pk>/
+    path('detail/<int:pk>/', watches_views.DetailWatchView.as_view(), name='watch_detail'),    # 127.0.0.1:8000/detail/<int:pk>/
+
+    # path('update/<int:pk>/', watches_views.update_watch, name='watch_update'),    # 127.0.0.1:8000/update/<int:pk>/
+    path('update/<int:pk>/', watches_views.UpdateWatchView.as_view(), name='watch_update'),    # 127.0.0.1:8000/update/<int:pk>/
+
+    # path('delete/<int:pk>/', watches_views.delete_watch, name='watch_delete'),    # 127.0.0.1:8000/delete/<int:pk>/
+    path('delete/<int:pk>/', watches_views.DeleteWatchView.as_view(), name='watch_delete'),    # 127.0.0.1:8000/delete/<int:pk>/
 ]
--- watches/views.py
+++ watches/views.py
@@ -1,3 +1,5 @@
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required 
 from django.shortcuts import get_object_or_404  # Məlumatları və ya error-u almaq üçündür.
 from django.contrib import messages             # Mesajlar üçündür.
@@ -10,29 +12,129 @@ from watches.models import Watch    # Model import edilir.
 from watches.forms import WatchForm # Form import edilir.
 
 
+                            # Function base views.
                             # Əsas funksliyalar yaradılır.
 
 # Bütün watch məlumatları.
-def all_watches(request):
-    watches = Watch.objects.all()   # Watch modelindəki bütün verailbleləri verir.
-    context = {
-        'watches': watches
-    }
-    return render(request, 'watches/all_watch.html', context)
+# def all_watches(request):
+#     watches = Watch.objects.all()   # Watch modelindəki bütün verailbleləri verir.
+#     context = {
+#         'watches': watches
+#     }
+#     return render(request, 'watches/all_watch.html', context)
 
 
-def my_watches(request):
-    my_watches = Watch.objects.filter(seller=request.user)
-    context = {
-        'my_watches': my_watches
-    }
-    return render(request, 'watches/my_watches.html', context)
+# Yalnız login olan sellerin watch-larını gətirən funksiya.
+# def my_watches(request):
+    # my_watches = Watch.objects.filter(seller=request.user)
+    # context = {
+    #     'my_watches': my_watches
+    # }
+    # return render(request, 'watches/my_watches.html', context)
 
 
 # Watch məlumatlarını daxil edildiyi form funksiyası.
-@login_required  # creat səhifəsindən öncə login hissəsinin açılmasını təmin edir 
-def create_watch(request):
-    if request.method == 'POST':   # Sorğunun POST olmasını yoxlayır.
+# @login_required  # creat səhifəsindən öncə login hissəsinin açılmasını təmin edir 
+# def create_watch(request):
+#     if request.method == 'POST':   # Sorğunun POST olmasını yoxlayır.
+#         form = WatchForm(request.POST, request.FILES)  # Post sorğusuna görə formu verir.
+#         if form.is_valid():     # Formun valuesinin doğruluğunu yoxlayır.
+#             new_form = form.save(commit=False)       # Formu python səviyyəsində save edir.
+#             new_form.seller = request.user        # Foruma useri daxil edir.
+#             form.save()         # Formu seyv edir.
+#             new_form.save()     # SQL səviyyəsində forumu seyv edirik.
+#             return redirect('all_watch')
+#         else:
+#             messages.error(request, "Yanlış əməliyyat")
+#             messages.error(request, form.errors)    # Formun errorlarını verir.
+#             messages.error(request, "MM/DD/YYYY")
+#             return redirect('watch_create')
+        
+#     else:
+#         watch_form = {
+#             'watch_form': WatchForm()
+#         }
+#         return render(request, 'watches/create_watch.html', watch_form)
+
+
+# Detalları göstərən funksiya.
+# def detail_watch(request, pk):
+#     watches = Watch.objects.select_related('seller').prefetch_related('categories')
+#     watch = get_object_or_404(watches, id=pk)
+
+#     watch_context = {
+#         'watch': watch
+#     }
+
+#     return render(request, 'watches/detail_watch.html', watch_context)
+
+
+# Update etmək üçün funksiya.
+# @login_required
+# def update_watch(request, pk):
+#     watches = Watch.objects.filter(seller=request.user).select_related('seller').prefetch_related('categories')            # Useri request-dəki userlə eyni olan watch-ları seçib alır. 
+#     watch = get_object_or_404(watches, id=pk)
+#     if request.method == 'POST':
+#         form = WatchForm(request.POST, request.FILES, instance=watch)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('watch_detail', watch.id)
+#         else:
+#             messages.error(request, form.errors)
+#             return redirect('watch_update', watch.id)
+#     else:
+#         form = WatchForm(instance=watch)
+#         context = {
+#             'form': form,
+#             'watch': watch
+#         }
+#         return render(request, 'watches/update_watch.html', context)
+
+
+# Silmə edtmək üçün funksiya.
+# @login_required
+# def delete_watch(request, pk):
+#     watches = Watch.objects.filter(seller=request.user)
+#     watch = get_object_or_404(watches, id=pk)
+#     watch.delete()      # Watch məlumatlarını silmək üçün method.
+#     messages.success(request, f'{pk} ID-li watch məlumatları silindi.') # Uğurlu mesajı.
+#     return redirect('all_watch')
+
+
+
+                        # Class base views.
+                        # View-ləri class-lar ilə yazırıq.
+
+class ListWatchView(View):
+    def get(self, request):
+        watches = Watch.objects.all()   # Watch modelindəki bütün verailbleləri verir.
+
+        context = {
+            'watches': watches
+        }
+        return render(request, 'watches/all_watch.html', context)
+    
+
+
+class MyWatchView(View):
+    def get(self, request):
+        my_watches = Watch.objects.filter(seller=request.user)
+        context = {
+            'my_watches': my_watches
+        }
+        
+        return render(request, 'watches/my_watches.html', context)
+    
+
+
+class CreateWatchView(LoginRequiredMixin, View):
+    def get(self, request):
+        watch_form = {
+            'watch_form': WatchForm()
+        }
+        return render(request, 'watches/create_watch.html', watch_form)
+
+    def post(self, request):
         form = WatchForm(request.POST, request.FILES)  # Post sorğusuna görə formu verir.
         if form.is_valid():     # Formun valuesinin doğruluğunu yoxlayır.
             new_form = form.save(commit=False)       # Formu python səviyyəsində save edir.
@@ -45,32 +147,53 @@ def create_watch(request):
             messages.error(request, form.errors)    # Formun errorlarını verir.
             messages.error(request, "MM/DD/YYYY")
             return redirect('watch_create')
-        
-    else:
-        watch_form = {
-            'watch_form': WatchForm()
+
+
+
+class DetailWatchView(LoginRequiredMixin, View):
+    def get_object(self, pk):
+        watches = Watch.objects.select_related('seller').prefetch_related('categories')
+        watch = get_object_or_404(watches, id=pk)
+        return watch
+
+    # def get(self, request, pk):
+    #     watch = self.get_object(pk=pk)
+    #     watch_context = {
+    #         'watch': watch
+    #     }
+
+    #     return render(request, 'watches/detail_watch.html', watch_context)
+
+    def post(self, request, pk):
+        watch = self.get_object(pk=pk)
+        watch_context = {
+            'watch': watch
         }
-        return render(request, 'watches/create_watch.html', watch_form)
+
+        return render(request, 'watches/detail_watch.html', watch_context)
+    
+    
+
+class UpdateWatchView(LoginRequiredMixin, View):
+    def get_object(self, request_user, pk):
+        watches = Watch.objects.filter(seller=request_user).select_related('seller').prefetch_related('categories')            # Useri request-dəki userlə eyni olan watch-ları seçib alır. 
+        watch = get_object_or_404(watches, id=pk)
+        return watch
+
+    def get(self, request, pk):
+        watch = self.get_object(request_user=request.user, pk=pk)
+
+        form = WatchForm(instance=watch)
+        update_context = {
+            'form': form,
+            'watch': watch
+        }
+        return render(request, 'watches/update_watch.html', update_context)
 
 
-# Detalları göstərən funksiya.
-def detail_watch(request, pk):
-    watches = Watch.objects.select_related('seller').prefetch_related('categories')
-    watch = get_object_or_404(watches, id=pk)
+    def post(self, request, pk):
+        watch = self.get_object(request_user=request.user, pk=pk)
 
-    watch_context = {
-        'watch': watch
-    }
-
-    return render(request, 'watches/detail_watch.html', watch_context)
-
-
-# Update etmək üçün funksiya.
-@login_required
-def update_watch(request, pk):
-    watches = Watch.objects.filter(seller=request.user).select_related('seller').prefetch_related('categories')            # Useri request-dəki userlə eyni olan watch-ları seçib alır. 
-    watch = get_object_or_404(watches, id=pk)
-    if request.method == 'POST':
         form = WatchForm(request.POST, request.FILES, instance=watch)
         if form.is_valid():
             form.save()
@@ -78,20 +201,20 @@ def update_watch(request, pk):
         else:
             messages.error(request, form.errors)
             return redirect('watch_update', watch.id)
-    else:
-        form = WatchForm(instance=watch)
-        context = {
-            'form': form,
-            'watch': watch
-        }
-        return render(request, 'watches/update_watch.html', context)
 
 
-# Silmə edtmək üçün funksiya.
-@login_required
-def delete_watch(request, pk):
-    watches = Watch.objects.filter(seller=request.user)
-    watch = get_object_or_404(watches, id=pk)
-    watch.delete()      # Watch məlumatlarını silmək üçün method.
-    messages.success(request, f'{pk} ID-li watch məlumatları silindi.') # Uğurlu mesajı.
-    return redirect('all_watch')
+class DeleteWatchView(LoginRequiredMixin, View):
+    def get_object(self, request_user, pk):
+        watches = Watch.objects.filter(seller=request_user)
+        watch = get_object_or_404(watches, id=pk)
+        return watch
+
+    def post(self, request, pk):
+        watch = self.get_object(request_user=request.user, pk=pk)
+
+        self.perform_delete(obj=watch)      # Watch məlumatlarını silmək üçün method.
+        messages.success(request, f'{pk} ID-li watch məlumatları silindi.') # Uğurlu mesajı.
+        return redirect('all_watch')
+    
+    def perform_delete(self, obj):
+        obj.delete()
